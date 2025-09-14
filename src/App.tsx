d90a986fe632c3a3286892c88d0b5958a52569fe
Matsonian <matsonian@yahoo.com>
2025-09-14 16:05:53 -0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -17,6 +17,8 @@ import MakerPage from './pages/MakerPage';
 import YouNeedListiclesPage from './pages/YouNeedListiclesPage';
 import ConfirmEmailPage from './pages/ConfirmEmailPage';
 import WelcomePage from './pages/WelcomePage';
+import ListicleDetailPage from './src/pages/ListicleDetailPage';
+
 
 
 function App() {
