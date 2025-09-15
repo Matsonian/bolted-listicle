b6e3382752ed1c7c27b9e4ac82a6e78cd37ac377
Matsonian <matsonian@yahoo.com>
2025-09-15 02:28:24 -0700

    Fix SignupPage Syntax Error

--- src/pages/SignupPage.tsx
+++ src/pages/SignupPage.tsx
@@ -143,6 +143,7 @@ export default function SignupPage() {
     } finally {
       setLoading(false);
     }
+  };
 
   return (
     <div className="min-h-screen flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8 bg-gray-50">
@@ -553,4 +554,3 @@ export default function SignupPage() {
       </div>
     </div>
   );
-}
