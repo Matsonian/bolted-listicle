7ce2a2057a4954d203ec0afeded4cb900e68fdb7
Matsonian <matsonian@yahoo.com>
2025-09-15 21:35:47 -0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,19 +4,15 @@ import Navigation from './components/Navigation';
 import Footer from './components/Footer';
 import HomePage from './pages/HomePage';
 import SearchPage from './pages/SearchPage';
-import LoginPage from './pages/LoginPage';
-import SignupPage from './pages/SignupPage';
-import ProfilePage from './pages/ProfilePage';
+
 import AboutPage from './pages/AboutPage';
 import ContactPage from './pages/ContactPage';
 import PrivacyPage from './pages/PrivacyPage';
 import BlogPage from './pages/BlogPage';
 import BlogPostPage from './pages/BlogPostPage';
-import EducationPage from './pages/EducationPage';
-import MakerPage from './pages/MakerPage';
+
 import YouNeedListiclesPage from './pages/YouNeedListiclesPage';
-import ConfirmEmailPage from './pages/ConfirmEmailPage';
-import WelcomePage from './pages/WelcomePage';
+
 import ListicleDetailPage from './pages/ListicleDetailPage';
 
 
@@ -30,19 +26,15 @@ function App() {
           <Routes>
             <Route path="/" element={<HomePage />} />
             <Route path="/search" element={<SearchPage />} />
-            <Route path="/login" element={<LoginPage />} />
-            <Route path="/signup" element={<SignupPage />} />
-            <Route path="/profile" element={<ProfilePage />} />
+  
             <Route path="/about" element={<AboutPage />} />
             <Route path="/contact" element={<ContactPage />} />
             <Route path="/privacy" element={<PrivacyPage />} />
             <Route path="/blog" element={<BlogPage />} />
             <Route path="/blog/:slug" element={<BlogPostPage />} />
-            <Route path="/education" element={<EducationPage />} />
-            <Route path="/maker" element={<MakerPage />} />
+     
             <Route path="/you-need-listicles" element={<YouNeedListiclesPage />} />
-            <Route path="/confirm-email" element={<ConfirmEmailPage />} />
-            <Route path="/welcome" element={<WelcomePage />} />
+
             <Route path="/listicle-detail/:url" element={<ListicleDetailPage />} />
 
 
