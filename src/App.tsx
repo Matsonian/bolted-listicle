a8d53e9d0d25c6753bc9466454e4639d698bf927
Matsonian <matsonian@yahoo.com>
2025-09-16 09:52:26 -0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,49 +1,47 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navigation from './components/Navigation';
-import Footer from './components/Footer';
-import HomePage from './pages/HomePage';
-import SearchPage from './pages/SearchPage';
-
-import AboutPage from './pages/AboutPage';
-import ContactPage from './pages/ContactPage';
-import PrivacyPage from './pages/PrivacyPage';
-import BlogPage from './pages/BlogPage';
-import BlogPostPage from './pages/BlogPostPage';
-
-import YouNeedListiclesPage from './pages/YouNeedListiclesPage';
-
-import ListicleDetailPage from './pages/ListicleDetailPage';
-
-
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { AuthProvider } from './contexts/AuthContext'
+import Navigation from './components/Navigation'
+import HomePage from './pages/HomePage'
+import SearchPage from './pages/SearchPage'
+import BlogPage from './pages/BlogPage'
+import SignupPage from './pages/SignupPage'
+import LoginPage from './pages/LoginPage'
+import ConfirmEmailPage from './pages/ConfirmEmailPage'
+import WelcomePage from './pages/WelcomePage'
+import ProfilePage from './pages/ProfilePage'
+import EducationPage from './pages/EducationPage'
+import ProtectedRoute from './components/ProtectedRoute'
 
 function App() {
   return (
     <Router>
-      <div className="min-h-screen bg-gray-50 flex flex-col">
-        <Navigation />
-        <main className="flex-grow">
+      <AuthProvider>
+        <div className="min-h-screen bg-white">
+          <Navigation />
           <Routes>
             <Route path="/" element={<HomePage />} />
             <Route path="/search" element={<SearchPage />} />
-  
-            <Route path="/about" element={<AboutPage />} />
-            <Route path="/contact" element={<ContactPage />} />
-            <Route path="/privacy" element={<PrivacyPage />} />
             <Route path="/blog" element={<BlogPage />} />
-            <Route path="/blog/:slug" element={<BlogPostPage />} />
-     
-            <Route path="/you-need-listicles" element={<YouNeedListiclesPage />} />
-
-            <Route path="/listicle-detail/:url" element={<ListicleDetailPage />} />
-
-
+            <Route path="/signup" element={<SignupPage />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/confirm-email" element={<ConfirmEmailPage />} />
+            <Route path="/welcome" element={<WelcomePage />} />
+            <Route path="/profile" element={
+              <ProtectedRoute>
+                <ProfilePage />
+              </ProtectedRoute>
+            } />
+            <Route path="/education" element={
+              <ProtectedRoute>
+                <EducationPage />
+              </ProtectedRoute>
+            } />
           </Routes>
-        </main>
-        <Footer />
-      </div>
+        </div>
+      </AuthProvider>
     </Router>
-  );
+  )
 }
 
-export default App;
+export default App
