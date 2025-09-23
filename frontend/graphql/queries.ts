d1c687f3dd3e33ba65dcf48df581cb36b886ee13
Matsonian <matsonian@yahoo.com>
2025-09-23 14:02:02 -0700

    Update queries.ts

--- frontend/graphql/queries.ts
+++ frontend/graphql/queries.ts
@@ -18,3 +18,44 @@ query User {
   }
 }
 `;
+
+export const SIGN_IN_MUTATION = gql`
+  mutation SignUpOrInWithPassword($email: String!, $password: String!) {
+    signUpOrInWithPassword(email: $email, password: $password) {
+      token
+      userId
+    }
+  }
+`;
+
+export const UPDATE_USER_MUTATION = gql`
+  mutation UpdateUser(
+    $firstName: String
+    $lastName: String
+    $isOnboarded: Boolean
+  ) {
+    updateUser(
+      firstName: $firstName
+      lastName: $lastName
+      isOnboarded: $isOnboarded
+    ) {
+      id
+      firstName
+      lastName
+      email
+      isOnboarded
+    }
+  }
+`;
+
+export const ME_QUERY = gql`
+  query Me {
+    me {
+      id
+      email
+      firstName
+      lastName
+      isOnboarded
+    }
+  }
+`;
