f052b86614d3aae58894a99258dae9bd949a4864
Matsonian <matsonian@yahoo.com>
2025-10-03 23:46:06 -0700

    Create page.tsx

---
+++ frontend/app/analyze/[url]/page.tsx
@@ -0,0 +1,10 @@
+'use client';
+
+export default function AnalyzePage() {
+  return (
+    <div className="min-h-screen p-8">
+      <h1>Analyze Page Works!</h1>
+      <p>If you see this, the routing is working.</p>
+    </div>
+  );
+}
