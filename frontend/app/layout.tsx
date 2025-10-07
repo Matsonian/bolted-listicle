abef3e1b1369d7a81bc41a926d83843233bcf376
Matsonian <matsonian@yahoo.com>
2025-10-07 08:55:31 -0700

    Update layout.tsx with SiteBehavior script

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -21,21 +21,44 @@ export default async function RootLayout({
 }: {
   children: React.ReactNode
 }) {
-
   const session = (await getServerSession(authOptions)) as Session | null;
-
+  
   return (
     <html lang="en">
+      <head>
+        <script
+          type="text/javascript"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function() {
+                try {
+                  if(window.location && window.location.search && window.location.search.indexOf('capture-sitebehaviour-heatmap') !== -1) {
+                    sessionStorage.setItem('capture-sitebehaviour-heatmap', '_');
+                  }
+                  var sbSiteSecret = 'a1f9c791-4145-4a6f-9c63-7c19f2b466f0';
+                  window.sitebehaviourTrackingSecret = sbSiteSecret;
+                  var scriptElement = document.createElement('script');
+                  scriptElement.defer = true;
+                  scriptElement.id = 'site-behaviour-script-v2';
+                  scriptElement.src = 'https://sitebehaviour-cdn.fra1.cdn.digitaloceanspaces.com/index.min.js?sitebehaviour-secret=' + sbSiteSecret;
+                  document.head.appendChild(scriptElement); 
+                }
+                catch (e) {console.error(e)}
+              })()
+            `
+          }}
+        />
+      </head>
       <body className={inter.className}>
         <AuthProvider session={session}>  
           <ApolloProviderWrapper> 
-          <div className="min-h-screen bg-white flex flex-col">
-            <Navigation />
-            <main className="flex-grow">
-              {children}
-            </main>
-            <Footer />
-          </div>
+            <div className="min-h-screen bg-white flex flex-col">
+              <Navigation />
+              <main className="flex-grow">
+                {children}
+              </main>
+              <Footer />
+            </div>
           </ApolloProviderWrapper>
         </AuthProvider>
       </body>
