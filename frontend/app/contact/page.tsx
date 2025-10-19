d67d45c0a772bf8b0ed681584a0da9264ee4fcfe
Matsonian <matsonian@yahoo.com>
2025-10-19 15:15:37 -0700

    Update contact page.tsx

--- frontend/app/contact/page.tsx
+++ frontend/app/contact/page.tsx
@@ -1,47 +1,6 @@
-'use client'
-
-import React, { useEffect } from 'react';
+import React from 'react';
 
 export default function ContactPage() {
-  useEffect(() => {
-    // Load Wufoo script
-    const script = document.createElement('script');
-    script.type = 'text/javascript';
-    script.src = 'https://secure.wufoo.com/scripts/embed/form.js';
-    script.async = true;
-    
-    script.onload = () => {
-      try {
-        // @ts-ignore - WufooForm is loaded by the external script
-        if (typeof WufooForm !== 'undefined') {
-          const z1s0iqi1it1udq = new WufooForm();
-          z1s0iqi1it1udq.initialize({
-            'userName': 'matsonian2',
-            'formHash': 'z1s0iqi1it1udq',
-            'autoResize': true,
-            'height': '517',
-            'async': true,
-            'host': 'wufoo.com',
-            'header': 'show',
-            'ssl': true
-          });
-          z1s0iqi1it1udq.display();
-        }
-      } catch (e) {
-        console.error('Wufoo form initialization error:', e);
-      }
-    };
-    
-    document.body.appendChild(script);
-    
-    return () => {
-      // Cleanup script on unmount
-      if (script.parentNode) {
-        script.parentNode.removeChild(script);
-      }
-    };
-  }, []);
-
   return (
     <div className="min-h-screen bg-gray-50 py-12">
       <div className="container mx-auto px-4">
@@ -51,7 +10,18 @@ export default function ContactPage() {
           </h1>
           
           {/* Wufoo Form Embed */}
-          <div id="wufoo-z1s0iqi1it1udq"></div>
+          <iframe 
+            height="517" 
+            title="Embedded Wufoo Form" 
+            allowTransparency={true}
+            frameBorder="0" 
+            scrolling="no" 
+            style={{width: '100%', border: 'none'}}
+            sandbox="allow-popups-to-escape-sandbox allow-top-navigation allow-scripts allow-popups allow-forms allow-same-origin" 
+            src="https://matsonian2.wufoo.com/embed/z1s0iqi1it1udq/"
+          >
+            <a href="https://matsonian2.wufoo.com/forms/z1s0iqi1it1udq/">Fill out my Wufoo form!</a>
+          </iframe>
         </div>
       </div>
     </div>
